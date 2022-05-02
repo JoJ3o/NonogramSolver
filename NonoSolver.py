885706b1895217dee7c139c940b15a5244a817d6
JoJ3o <42371079+JoJ3o@users.noreply.github.com>
2022-05-02 12:05:25 +0200

    Find & Place Overlap clues

--- NonoSolver.py
+++ NonoSolver.py
@@ -1,5 +1,5 @@
 size = 5
-clues = {"xAxis": {1 : [1], 2: [1, 3], 3: [3], 4: [2, 1], 5: [2]}, "yAxis": {1: [1, 2], 2: [2], 3: [2, 2], 4: [2], 5: [3]}}
+clues = {"xAxis": {1 : [1], 2: [1, 3], 3: [3], 4: [2, 1], 5: [2]}, "yAxis": {1: [1, 2], 2: [2], 3: [3], 4: [2], 5: [3]}}
 
 # for x in range(size):
 #     columnInput = input("X" + str(x + 1) + ": ").split(" ")
@@ -21,7 +21,7 @@ def genMap(size):
 
 map = genMap(size)
 
-def findGuarantee(clue):
+def isGuarantee(clue):
 	clueSum = sum(clue) + (len(clue) - 1)
 	if clueSum == size:
 		return True
@@ -32,24 +32,39 @@ def placeGuarantee(axis, index, clue):
 	for num in clue:
 		for i in range(num):
 			if axis == "xAxis":
-				map[i + pos][index - 1] = 1
+				map[pos + i][index - 1] = 1
 			if axis == "yAxis":
-				map[index - 1][i + pos] = 1
+				map[index - 1][pos + i] = 1
 		# +1 for empty cell between 2 blocks
 		pos += num + 1
 
+def isOverlap(clue):
+	if max(clue) > size/2:
+		return True
+	return False
 
-
+def placeOverlap(axis, index, clue):
+	pos = size - max(clue)
+	overlap = int((max(clue) - size/2) * 2)
+	print(overlap)
+	for i in range(overlap):
+		if axis == "xAxis":
+			map[pos + i][index - 1] = 1
+		if axis == "yAxis":
+			map[index - 1][pos + i] = 1
 
 for axisName, axisClues in clues.items():
 	print(axisName, axisClues)
 	for index, clue in axisClues.items():
-		print(index, clue, findGuarantee(clue))
-		if (findGuarantee(clue)):
+		print(index, clue, (max(clue) - size/2) * 2)
+		if (isGuarantee(clue)):
 			placeGuarantee(axisName, index, clue)
+		elif (isOverlap(clue)):
+			placeOverlap(axisName, index, clue)
 
 for y in map:
 	print(y)
+
 
         
 
