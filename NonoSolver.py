2f10f542c7e94062a28fdc8baf5232db95c624c0
JoJ3o <42371079+JoJ3o@users.noreply.github.com>
2022-05-04 21:55:41 +0200

    Code cleanup

--- NonoSolver.py
+++ NonoSolver.py
@@ -1,5 +1,10 @@
 size = 5
 clues = {"xAxis": {0 : [1], 1: [1], 2: [3], 3: [2, 1], 4: [1, 2]}, "yAxis": {0: [1, 3], 1: [2], 2: [3], 3: [2], 4: [2, 1]}}
+map = []
+for x in range(size):
+	map.append([])
+	for y in range(size):
+		map[x].append(0)
 
 # for x in range(size):
 #     columnInput = input("X" + str(x + 1) + ": ").split(" ")
@@ -11,23 +16,9 @@ clues = {"xAxis": {0 : [1], 1: [1], 2: [3], 3: [2, 1], 4: [1, 2]}, "yAxis": {0: 
 #     rowItems = [int(i) for i in rowInput]
 #     map["yAxis"].append(rowItems)
 
-def genMap(size):
-	map = []
-	for x in range(size):
-		map.append([])
-		for y in range(size):
-			map[x].append(0)
-	return map
-
-map = genMap(size)
-
-def isGuarantee(clue):
-	clueSum = sum(clue) + (len(clue) - 1)
-	if clueSum == size:
-		return True
-	return False
-
 def placeGuarantee(axis, index, clue):
+	if sum(clue) + (len(clue) - 1) == size: return
+
 	seperators = [idx * 2 + num for idx, num in enumerate(clue[:-1])]
 	for i in range(size):
 		cellType = 1 if (not (i in seperators)) else 2
@@ -37,12 +28,9 @@ def placeGuarantee(axis, index, clue):
 			map[index][i] = cellType
 	clues[axis][index].clear()
 
-def isOverlap(clue):
-	if max(clue) > size/2:
-		return True
-	return False
-
 def placeOverlap(axis, index, clue):
+	if max(clue) <= size/2: return
+	
 	pos = size - max(clue)
 	overlap = int((max(clue) - size/2) * 2)
 	for i in range(overlap):
@@ -51,7 +39,7 @@ def placeOverlap(axis, index, clue):
 		elif axis == "yAxis":
 			map[index][pos + i] = 1
 
-def isSide(axis, index, clue):
+def placeEdge(axis, index, clue):
 	if axis == "xAxis":
 		for cellIdx in range(size):
 			if map[cellIdx][index] == 1:
@@ -75,35 +63,37 @@ def isSide(axis, index, clue):
 						map[index][-(i+1)] = 1
 					map[index][-(clue[-1] + 1)] = 2
 
-def printMap(axis, clue):
+def printMap(axis, index, clue):
 	print("\n")
-	print(axis + ":", clue)
+	print(axis + ":", index + 1, clue)
 	for y in map:
 		print(y)
 
 for axisName, axisClues in clues.items():
 	for index, clue in axisClues.items():
-		if (isGuarantee(clue)):
-			placeGuarantee(axisName, index, clue)
-		elif (isOverlap(clue)):
-			placeOverlap(axisName, index, clue)
+		placeGuarantee(axisName, index, clue)
+		placeOverlap(axisName, index, clue)
 		
 		if not clue:
 			continue
-		isSide(axisName, index, clue)
-		printMap(axisName, clue)
+		placeEdge(axisName, index, clue)
+		printMap(axisName, index, clue)
 
 for axisName, axisClues in clues.items():
 	for index, clue in axisClues.items():
 		if not clue:
 			continue
-		isSide(axisName, index, clue)
-		printMap(axisName, clue)
+		placeEdge(axisName, index, clue)
+		printMap(axisName, index, clue)
 
-print(clues)
+while any([not all(row) for row in map]):
+	oldMap = map
 
-# while any([any(row) for row in map]):
-# 	pass
+
+
+	if map == oldMap:
+		print("Error: looploop")
+		break
         
 
 
